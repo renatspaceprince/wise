4861c47b19da84eecf4c277d94aad797cd8449b5
renatspaceprince <renatspaceprince@gmail.com>
2024-04-14 15:51:05 +0300

    добавил страницу shop

--- main.js
+++ main.js
@@ -12,16 +12,3 @@ logoRed.addEventListener('mouseout', function () {
   logo.style.display = 'block'
   logoRed.style.display = 'none'
 })
-
-const cards = document.querySelectorAll('.bw-a')
-const cardImgs = document.querySelectorAll('.bw-img')
-
-cards.forEach(function (card, index) {
-  card.addEventListener('mouseover', function () {
-    cardImgs[index].style.filter = 'none'
-  })
-
-  card.addEventListener('mouseout', function () {
-    cardImgs[index].style.filter = 'grayscale(100%)'
-  })
-})
